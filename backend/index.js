971c61a95477adcccd0ad1d4f1793ba6b77a87eb
Devesh Chandra <yashdev2712@gmail.com>
2025-02-11 13:50:23 +0530

    client can connect to the server

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+import { Server } from "socket.io";
+import http from "http";
+
 import connectToDb from "./config/Db.js";
 import authRouter from "./routes/auth.routes.js";
 
@@ -11,17 +14,33 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
 dotenv.config();
+
+io.on("connection", (socket) => {
+  console.log("user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected:", socket.id);
+  });
+});
 
 app.use("/api/auth", authRouter);
 
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("hii there");
-});
+// app.get("/", (req, res) => {
+//   res.send("hii there");
+// });
 
-app.listen(port, () => {
-  connectToDb();
+server.listen(port, () => {
+  // connectToDb();
   console.log(`server is running on the port: ${port}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { io } from "socket.io-client";
 
 const App = () => {
-  return <div></div>;
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const socket = io("http://localhost:3000");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (input) {
+      setList((prev) => [...prev, input]);
+      setInput("");
+    }
+    return;
+  };
+
+  return (
+    <div className="w-screen h-screen bg-gray-500 flex justify-center items-center">
+      <div className="w-1/2 h-3/4 bg-white rounded-lg flex flex-col justify-between items-center p-4">
+        <div className="w-full overflow-y-auto">
+          {list && (
+            <div className="flex justify-center items-center w-full">
+              <ul className="flex flex-col justify-center items-center w-full gap-2">
+                {list.map((items, index) => (
+                  <li
+                    key={index}
+                    className="text-xl w-full bg-black/60 text-white px-4 py-2 rounded-lg"
+                  >
+                    {items}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+        <form action="" className="flex justify-between gap-2 w-full">
+          <input
+            type="text"
+            placeholder="Type here..."
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            className="border bg-gray-200 w-full rounded px-2 outline-none text-black"
+          />
+          <button
+            type="submit"
+            onClick={onSubmit}
+            className="bg-black text-white rounded p-2"
+          >
+            send
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default App;
